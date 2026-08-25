5276e2747d9d2665b79e5553990dfe32126dc9c1
Softjit Singh <besofttech8@gmail.com>
2026-08-25 18:21:30 +0530

    put and delete api done for todo

---
+++ app/api/todo/[slug]/route.ts
@@ -0,0 +1,42 @@
+import { connectdb } from "@/lib/mongodb";
+import Todo from "@/lib/todo.model";
+import { NextResponse } from "next/server";
+
+
+export async function PUT (req : Request, {params} : {params : {slug : string}}){
+
+    await connectdb()
+    const {slug} = await params;
+
+    const body = await req.json()
+    const update = await Todo.findByIdAndUpdate(slug, body, { new : true, runValidators : true})
+
+    if (!update){
+        return NextResponse.json({ success : false, message : "not updated"}, {status : 400})
+    }
+return NextResponse.json({message : "updated", data : update}, {status : 200})
+
+}
+
+
+export async function DELETE (req : Request, 
+    {params} : {params : {slug : string}}
+) {
+    await connectdb();
+
+    const {slug} = await params;
+    
+    const del = await Todo.findByIdAndDelete(slug)
+
+    if(!del){
+        return NextResponse.json({
+            message :  "Not deleted"
+        })
+    }
+
+    return NextResponse.json({
+        message : "Deleted",
+        data : del
+    })
+    
+}
